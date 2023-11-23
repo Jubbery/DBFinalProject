f553fc0c7675282d1317ff5ba04eb6af8d95b21e
O.J. Parker <78820644+codenodejode74@users.noreply.github.com>
2023-11-22 20:06:55 -0500

    Update data.sql

--- server/data.sql
+++ server/data.sql
@@ -71,3 +71,40 @@ CREATE TABLE Courses (
   course_code VARCHAR(250),
   semester VARCHAR(250)
 );
+
+--
+
+-- Insert into Users
+INSERT INTO Users(user_id, fname, lname, email, hashed_pass)
+VALUES (1, 'John', 'Doe', 'john.doe@example.com', 'hashedpassword1'),
+     (2, 'Jane', 'Doe', 'jane.doe@example.com', 'hashedpassword2');
+
+-- Insert into Tasks
+INSERT INTO Tasks(user_id, task_name, start_date, deadline, priority_level, task_status, created_at, note, task_type)
+VALUES (1, 'Task 1', '2022-01-01', '2022-01-31', 'High', 'Not-Started', CURRENT_TIMESTAMP, 'This is a note', 'Exam'),
+     (2, 'Task 2', '2022-02-01', '2022-02-28', 'Medium', 'In-Progress', CURRENT_TIMESTAMP, 'This is another note', 'Project');
+
+-- Insert into Tag
+INSERT INTO Tag(tag_name, course_id, tag_description)
+VALUES ('Tag1', 'Course1', 'This is a tag description'),
+     ('Tag2', 'Course2', 'This is another tag description');
+
+-- Insert into TaskTag
+INSERT INTO TaskTag(task_id, tag_name)
+VALUES (1, 'Tag1'),
+     (2, 'Tag2');
+
+-- Insert into Notifications
+INSERT INTO Notifications(user_id, message, notification_status, created_at, remind_at)
+VALUES (1, 'This is a notification', 'Unread', CURRENT_TIMESTAMP, '2022-01-15 12:00:00'),
+     (2, 'This is another notification', 'Read', CURRENT_TIMESTAMP, '2022-02-15 12:00:00');
+
+-- Insert into TaskNotification
+INSERT INTO TaskNotification(task_id, notification_id)
+VALUES (1, 1),
+     (2, 2);
+
+-- Insert into Courses
+INSERT INTO Courses(course_id, tag_name, description, course_code, semester)
+VALUES ('Course1', 'Tag1', 'This is a course description', 'CSE101', 'Spring 2022'),
+     ('Course2', 'Tag2', 'This is another course description', 'CSE102', 'Spring 2022');
